88252ef063df5d8f9909637c1f4e70e681024a25
Bijesh O S <bijeshos@users.noreply.github.com>
2020-03-11 15:03:43 +0530

    removed unwanted comments

--- main.go
+++ main.go
@@ -43,36 +43,9 @@ func main() {
 		micro.Metadata(map[string]string{
 			"type": "helloworld",
 		}),
-
-		// Setup some flags. Specify --run_client to run the client
-
-		// Add runtime flags
-		// We could do this below too
-		/*micro.Flags(cli.BoolFlag{
-			Name:  "run_client",
-			Usage: "Launch the client",
-		}),*/
 	)
 
-	// Init will parse the command line flags. Any flags set will
-	// override the above settings. Options defined here will
-	// override anything set on the command line.
-	service.Init(
-	// Add runtime action
-	// We could actually do this above
-	/*micro.Action(func(c *cli.Context) {
-		if c.Bool("run_client") {
-			runClient(service)
-			os.Exit(0)
-		}
-
-	}),*/
-
-	)
-
-	// By default we'll run the server unless the flags catch us
-
-	// Setup the server
+	service.Init()
 
 	// Register handler
 	proto.RegisterGreeterHandler(service.Server(), new(Greeter))
